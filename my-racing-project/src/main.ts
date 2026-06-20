9654755865bd919559e224abcd316a6484085a65
tekliukkateryna <teklukkaterina@gmail.com>
2026-06-20 11:59:01 +0300

    feat(render): enable track visualization loop

--- my-racing-project/src/main.ts
+++ my-racing-project/src/main.ts
@@ -7,23 +7,30 @@ async function init() {
     // 1. Спочатку чекаємо на ініціалізацію
     await Promise.all([setupRender(), appInitialized]);
     // 2. Потім запускаємо цикл
-    animate(lastTime);
     console.log("Initilized...");
+
+    requestAnimationFrame(animate);
 }
+
+let totalDistance = 0; 
+const TRACK_LENGTH = 1000;
 
 function animate(currentTime: number) {
     requestAnimationFrame(animate);
     let dt = (currentTime - lastTime) / 1000.0; // Convert to seconds
     lastTime = currentTime;
-    
     // Cap delta time to prevent physics explosions on background tab focus shifts
     if (dt > 0.1) dt = 0.1;
+
     const state = simulation(dt);
 
     if (state) {
-        runRender(state);
-    }
 
+    totalDistance += state.vx * dt; 
+    // t (від 0 до 1)
+    const t = (totalDistance % TRACK_LENGTH) / TRACK_LENGTH; 
+        runRender(t); 
+    }
 
 }
 
